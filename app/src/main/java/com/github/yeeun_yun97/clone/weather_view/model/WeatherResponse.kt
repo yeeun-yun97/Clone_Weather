c45186e00d6dd29a069fd23c08e71b6b0b8395f9
yyn9704@gmail.com <yyn9704@gmail.com>
2022-04-18 20:59:00 +0900

    added serialized names (heard about proguard?)

--- app/src/main/java/com/github/yeeun_yun97/clone/weather_view/model/WeatherResponse.kt
+++ app/src/main/java/com/github/yeeun_yun97/clone/weather_view/model/WeatherResponse.kt
@@ -1,7 +1,12 @@
 package com.github.yeeun_yun97.clone.weather_view.model
 
+import com.google.gson.annotations.SerializedName
+
 data class WeatherResponse(
+    @SerializedName("weather")
     val weather:List<Map<String,String>>,
+    @SerializedName("main")
     val main:Map<String,String>,
+    @SerializedName("code")
     val code: Int
 )
